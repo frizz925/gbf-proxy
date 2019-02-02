c43c7c88b36cd99084c93ae954d4bb871b95556b
Izra Faturrahman <Frizz925@hotmail.com>
2019-02-03 01:47:25 +0700

    [nginx] Add Terraform config

---
+++ nginx/config.tf
@@ -0,0 +1,29 @@
+provider "docker" {
+    host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "nginx" {
+    name = "nginx:1.15"
+    keep_locally = true
+}
+
+resource "docker_container" "nginx-server" {
+    name = "nginx-server"
+    image = "${docker_image.nginx.latest}"
+
+    ports {
+        internal = 80
+        external = 8080
+    }
+
+    ports {
+        internal = 443
+        external = 4443
+    }
+
+    volumes {
+        host_path = "${path.cwd}/conf.d"
+        container_path = "/etc/nginx/conf.d"
+        read_only = true
+    }
+}
