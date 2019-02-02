a225bf1dd39a8a9618abc3eaf20b8e1ad8898196
Izra Faturrahman <Frizz925@hotmail.com>
2019-02-03 04:34:58 +0700

    [golang] Add arguments parsing

--- golang/main.go
+++ golang/main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"strconv"
 
 	"github.com/Frizz925/gbf-proxy/golang/proxy"
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: gbf-proxy PORT [HOST]")
+	}
+	host := "0.0.0.0"
+	port, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatal("Port must be a number!")
+	}
+	if len(os.Args) >= 3 {
+		host = os.Args[1]
+	}
+	addr := fmt.Sprintf("%s:%d", host, port)
 	s := proxy.NewServer()
-	l, err := s.Open("localhost:8000")
+	l, err := s.Open(addr)
 	if err != nil {
 		panic(err)
 	}
