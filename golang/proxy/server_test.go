4bde31a3222fa9970853dcdc488485e6f78f5256
Izra Faturrahman <Frizz925@hotmail.com>
2019-02-03 02:13:11 +0700

    [golang] Refactor test case

--- golang/proxy/server_test.go
+++ golang/proxy/server_test.go
@@ -1,38 +1,69 @@
 package proxy
 
 import (
+	"net"
 	"net/http"
 	"net/url"
+	"os"
 	"testing"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-func TestServer(t *testing.T) {
+type TestState struct {
+	Listener net.Listener
+	URL      string
+}
+
+var state *TestState
+
+func TestMain(m *testing.M) {
 	s := NewServer()
-	l, err := s.Start("localhost:0")
+	l, err := s.Open("localhost:0")
 	if err != nil {
-		t.Fatal(err)
+		panic(err)
 	}
+	defer s.Close()
 	addr := l.Addr().String()
-	proxyUrl, err := url.Parse("http://" + addr)
-	if err != nil {
-		t.Fatal(err)
+	url := "http://" + addr
+	state = &TestState{
+		Listener: l,
+		URL:      url,
 	}
-	client := http.Client{
-		Transport: &http.Transport{
-			Proxy: http.ProxyURL(proxyUrl),
-		},
-	}
-	res, err := client.Get("http://game.granbluefantasy.jp")
+	os.Exit(m.Run())
+}
+
+func TestForbidden(t *testing.T) {
+	res, err := http.Get(state.URL)
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer res.Body.Close()
 	code := res.StatusCode
-	if code != 200 {
-		t.Fatalf("HTTP error got status code %d", code)
+	if code != 403 {
+		t.Fatalf("Request is not forbidden! Status code: %d", code)
 	}
+}
+
+func TestAllowed(t *testing.T) {
+	proxyURL, err := url.Parse(state.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	c := http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(proxyURL),
+		},
+	}
+	req, err := http.NewRequest("GET", "http://game.granbluefantasy.jp", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	res, err := c.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer res.Body.Close()
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
 		t.Fatal(err)
