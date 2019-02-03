59fd67a64b7ebec84980374ea16bd103acbd11f2
Izra Faturrahman <Frizz925@hotmail.com>
2019-02-03 16:53:03 +0700

    [golang] Update entrypoint to select the service to run

--- golang/main.go
+++ golang/main.go
@@ -5,28 +5,66 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 
+	"github.com/Frizz925/gbf-proxy/golang/controller"
+	"github.com/Frizz925/gbf-proxy/golang/lib"
 	"github.com/Frizz925/gbf-proxy/golang/proxy"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		log.Fatal("Usage: gbf-proxy PORT [HOST]")
+	if len(os.Args) < 3 {
+		fatal("Usage: gbf-proxy <service> <address> [args ...]")
 	}
-	host := "0.0.0.0"
-	port, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		log.Fatal("Port must be a number!")
+	name := os.Args[1]
+	addr := os.Args[2]
+	if !strings.Contains(addr, ":") {
+		port, err := strconv.Atoi(addr)
+		if err != nil {
+			fatal("Address must be either a port or host:port format")
+		}
+		addr = fmt.Sprintf("0.0.0.0:%d", port)
 	}
-	if len(os.Args) >= 3 {
-		host = os.Args[1]
+	switch name {
+	case "controller":
+		controllerService(addr)
+	case "proxy":
+		proxyService(addr)
+	default:
+		fatalf("Unknown service '%s'", name)
 	}
-	addr := fmt.Sprintf("%s:%d", host, port)
-	s := proxy.NewServer()
+}
+
+func controllerService(addr string) {
+	runService("Controller", controller.NewServer(), addr)
+}
+
+func proxyService(addr string) {
+	if len(os.Args) < 4 {
+		fatal("Usage: gbf-proxy proxy <address> <backend-address> [args ...]")
+	}
+	backendAddr := os.Args[3]
+	log.Printf("Using %s as backend", backendAddr)
+	runService("Proxy", proxy.NewServer(backendAddr), addr)
+}
+
+func runService(name string, s lib.Server, addr string) {
 	l, err := s.Open(addr)
 	if err != nil {
 		panic(err)
 	}
-	log.Println("Proxy server listening at " + l.Addr().String())
-	s.WaitGroup.Wait()
+	log.Printf("%s service listening at %s\n", name, l.Addr().String())
+	s.WaitGroup().Wait()
+}
+
+func fatalf(format string, args ...interface{}) {
+	fatal(fmt.Sprintf(format, args...))
+}
+
+func fatal(message string) {
+	_, err := os.Stderr.WriteString(message)
+	if err != nil {
+		panic(err)
+	}
+	os.Exit(1)
 }
