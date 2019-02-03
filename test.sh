4b9e5853fa8d2bb58c5824543810b0b6c7c0e783
Izra Faturrahman <Frizz925@hotmail.com>
2019-02-03 16:52:22 +0700

    [travis-ci] Update test script

--- test.sh
+++ test.sh
@@ -10,24 +10,31 @@ echo "OK"
 
 # Testing using external tool
 # Setup
-echo "Spinning up server at port :8000..."
-go run ./main.go 8000 &
-SERVER_PID=$!
-echo "Server PID: $SERVER_PID"
+echo "Spinning up controller service at port localhost:8000..."
+go run ./main.go controller localhost:8000 &
+CONTROLLER_PID=$!
+
+echo "Spinning up proxy service at port localhost:8080..."
+go run ./main.go proxy localhost:8080 localhost:8000 &
+PROXY_PID=$!
+
+cleanup() {
+    echo "Killing servers..."
+    pkill -9 -P $CONTROLLER_PID > /dev/null 2>&1 || exit 0
+    pkill -9 -P $PROXY_PID > /dev/null 2>&1 || exit 0
+    echo "Done."
+}
+trap cleanup EXIT
+
 sleep 3
-pkill -0 -P $SERVER_PID > /dev/null 2>&1 || (echo "Server fails to run!" >&2 && exit 1)
+pkill -0 -P $CONTROLLER_PID > /dev/null 2>&1 || (echo "Controller service fails to run!" >&2 && exit 1)
+pkill -0 -P $PROXY_PID > /dev/null 2>&1 || (echo "Proxy service fails to run!" >&2 && exit 1)
 
 # Test allowed
 printf "Testing allowed host... "
-curl -six http://localhost:8000 game.granbluefantasy.jp | grep -q "グランブルーファンタジー" && echo "OK"
+curl -six http://localhost:8080 game.granbluefantasy.jp | grep -q "グランブルーファンタジー" && echo "OK"
 # Test forbidden
 printf "Testing forbidden host... "
-curl -six http://localhost:8000 github.com | grep -q "403 Forbidden" && echo "OK"
+curl -six http://localhost:8080 github.com | grep -q "403 Forbidden" && echo "OK"
 
-# Teardown
-echo "Killing server..."
-pkill -9 -P $SERVER_PID > /dev/null 2>&1
-
-# Done
-echo "Done."
 cd "$CWD"
