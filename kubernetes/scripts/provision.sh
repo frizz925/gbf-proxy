6ccdd97a0e30a097b2058c074fb42e323f83d8e1
Izra Faturrahman <Frizz925@hotmail.com>
2019-02-17 15:45:53 +0700

    [k8s] Remove tarball during provision

--- kubernetes/scripts/provision.sh
+++ kubernetes/scripts/provision.sh
@@ -5,6 +5,7 @@ CWD=$(pwd)
 SCRIPT_DIR=$(dirname "$0")
 FILES_DIR=$(realpath "$SCRIPT_DIR/../files")
 PROJECT_DIR=$(realpath "$1")
+TAR_PATH="$FILES_DIR/gbf-proxy.tar.gz"
 
 cleanup() {
     cd "$CWD"
@@ -17,4 +18,7 @@ make deps
 make test
 make build-linux
 
-tar -czf "$FILES_DIR/gbf-proxy.tar.gz" Dockerfile bin
+if [ -f "$TAR_PATH" ]; then
+    rm "$TAR_PATH"
+fi
+tar -czf "$TAR_PATH" Dockerfile bin/gbf-proxy-linux-amd64
