90a9a7efcaf4f7196845be81fda6be4552b4d56b
Izra Faturrahman <Frizz925@hotmail.com>
2019-02-17 15:46:05 +0700

    [k8s] Enable required sysctl

--- kubernetes/scripts/network-setup.sh
+++ kubernetes/scripts/network-setup.sh
@@ -11,5 +11,8 @@ fi
 FLANNEL_VERSION="v0.11.0"
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
+echo "Enabling bridged traffic to iptables' chains..."
+sysctl net.bridge.bridge-nf-call-iptables=1
+
 echo "Installing Flannel add-on..."
 kubectl apply -f "https://raw.githubusercontent.com/coreos/flannel/${FLANNEL_VERSION}/Documentation/kube-flannel.yml"
