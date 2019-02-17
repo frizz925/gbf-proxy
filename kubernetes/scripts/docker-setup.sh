d32076a0e1bd23afd973155cf4499c371096a0c8
Izra Faturrahman <Frizz925@hotmail.com>
2019-02-17 16:13:17 +0700

    [k8s] Fix Docker image checking logic

--- kubernetes/scripts/docker-setup.sh
+++ kubernetes/scripts/docker-setup.sh
@@ -7,7 +7,7 @@ mkdir -p /tmp/gbf-proxy
 tar -C /tmp/gbf-proxy -f /tmp/gbf-proxy.tar.gz -xz
 
 cd /tmp/gbf-proxy
-if docker images -q gbf-proxy:latest > /dev/null 2>&1; then
+if [ -n "$(docker images -q gbf-proxy:latest)" ]; then
     docker rmi gbf-proxy:latest
 fi
 docker build -qt gbf-proxy:latest .
