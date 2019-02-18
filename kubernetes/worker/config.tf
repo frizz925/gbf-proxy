8a8ff19146274d89f49da287c2c500b441def184
Izra Faturrahman <Frizz925@hotmail.com>
2019-02-18 15:47:41 +0700

    [k8s] Remove default values from config.tf

--- kubernetes/worker/config.tf
+++ kubernetes/worker/config.tf
@@ -1,30 +1,25 @@
 variable "worker_hosts" {
   type = "list"
-  default = [
-    "172.17.8.101",
-    "172.17.8.102",
-    "172.17.8.103"
-  ]
+}
+
+variable "worker_names" {
+  type = "list"
 }
 
 variable "worker_pvt_key" {
   type = "string"
-  default = "~/.vagrant.d/insecure_private_key"
 }
 
 variable "kube_apiserver" {
   type = "string"
-  default = "172.17.8.1:6443"
 }
 
 variable "kube_token" {
   type = "string"
-  default = "h4qwku.jjralldm8fnuc5qw"
 }
 
 variable "kube_hash" {
   type = "string"
-  default = "76e3d44c248734240b53baf5a1842b9e04abe9b9f2f8c4bcec4373fa605371a0"
 }
 
 variable "project_dir" {
