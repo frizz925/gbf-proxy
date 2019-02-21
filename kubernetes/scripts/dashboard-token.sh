47327ec8a972d4d12d0ecc830ae6ad87d21df8ee
Izra Faturrahman <Frizz925@hotmail.com>
2019-02-21 23:26:41 +0700

    [k8s] Add script for getting dashboard token

---
+++ kubernetes/scripts/dashboard-token.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SCRIPTS_DIR=$(realpath $(dirname $0))
+DEPLOYMENT_DIR=$(realpath $SCRIPTS_DIR/../deployment)
+DASHBOARD_USER=$(kubectl get secret -n kube-system | grep admin-user | awk '{ print $1 }')
+if [ -z "$DASHBOARD_USER" ]; then
+    exit 1
+else
+    kubectl describe secret -n kube-system $DASHBOARD_USER | awk '/token:/ { print $2 }'
+fi
