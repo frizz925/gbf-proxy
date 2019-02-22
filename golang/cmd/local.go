db3c421f0e9f890894618acb6ffb88f536b73f47
Izra Faturrahman <Frizz925@hotmail.com>
2019-02-23 03:48:37 +0700

    [golang] Add flags for local mode

--- golang/cmd/local.go
+++ golang/cmd/local.go
@@ -3,8 +3,9 @@ package cmd
 import (
 	"fmt"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
-	"strconv"
 
 	"github.com/Frizz925/gbf-proxy/golang/controller"
 
@@ -27,33 +28,54 @@ var localCmd = &cobra.Command{
 		if _, found := os.LookupEnv("GBF_PROXY_DEBUG"); !found {
 			log.SetOutput(new(devNull))
 		}
+		extProxyAddr, err := cmd.PersistentFlags().GetString("external-proxy")
+		if err != nil {
+			panic(err)
+		}
+		httpClient := http.DefaultClient
+		if extProxyAddr != "" {
+			proxyURL, err := url.Parse(extProxyAddr)
+			if err != nil {
+				panic(err)
+			}
+			httpClient = &http.Client{
+				Transport: &http.Transport{
+					Proxy: http.ProxyURL(proxyURL),
+				},
+			}
+			fmt.Printf("Using external proxy at %s\n", proxyURL.String())
+		}
 
-		cacheServer := cache.New(&cache.ServerConfig{})
+		cacheServer := cache.New(&cache.ServerConfig{
+			HttpClient: httpClient,
+		})
 		l, err := cacheServer.Open("127.0.0.1:0")
 		if err != nil {
 			panic(err)
 		}
 
 		controllerServer := controller.New(&controller.ServerConfig{
-			CacheAddr: l.Addr().String(),
+			CacheAddr:     l.Addr().String(),
+			DefaultClient: httpClient,
 		})
 		l, err = controllerServer.Open("127.0.0.1:0")
 		if err != nil {
 			panic(err)
 		}
 
+		host, err := cmd.PersistentFlags().GetString("host")
+		if err != nil {
+			panic(err)
+		}
+		port, err := cmd.PersistentFlags().GetInt("port")
+		if err != nil {
+			panic(err)
+		}
+
+		proxyAddr := fmt.Sprintf("%s:%d", host, port)
 		proxyServer := proxy.New(&proxy.ServerConfig{
 			BackendAddr: l.Addr().String(),
 		})
-		port := 0
-		if len(args) >= 1 {
-			port, err = strconv.Atoi(args[0])
-			if err != nil {
-				panic(err)
-			}
-		}
-
-		proxyAddr := fmt.Sprintf("127.0.0.1:%d", port)
 		l, err = proxyServer.Open(proxyAddr)
 		if err != nil {
 			panic(err)
@@ -66,5 +88,9 @@ var localCmd = &cobra.Command{
 }
 
 func init() {
+	localCmd.PersistentFlags().String("host", "localhost", "Host the local proxy should listen at")
+	localCmd.PersistentFlags().IntP("port", "p", 8088, "Port the local proxy should listen at")
+	localCmd.PersistentFlags().StringP("external-proxy", "e", "", "External proxy address to use")
+	localCmd.PersistentFlags()
 	rootCmd.AddCommand(localCmd)
 }
