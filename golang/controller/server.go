b786d9f8f2b9401bddec09b1bcea87c763ee8679
Izra Faturrahman <Frizz925@hotmail.com>
2019-02-25 07:02:57 +0700

    [golang] Add WebSocket capability

--- golang/controller/server.go
+++ golang/controller/server.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"net"
 	"net/http"
@@ -9,7 +11,7 @@ import (
 	"sync"
 	"time"
 
-	"github.com/Frizz925/gbf-proxy/golang/lib/logging"
+	"github.com/gorilla/websocket"
 
 	"github.com/Frizz925/gbf-proxy/golang/cache"
 	"github.com/Frizz925/gbf-proxy/golang/lib"
@@ -36,25 +38,44 @@ type Server struct {
 	cache          *http.Client
 	cacheAvailable bool
 	lock           *sync.Mutex
-	logger         *logging.Logger
+	upgrader       *websocket.Upgrader
+}
+
+type RequestError struct {
+	base       error
+	StatusCode int
+	Message    string
+}
+
+func NewRequestError(code int, message string, err error) *RequestError {
+	return &RequestError{
+		base:       err,
+		StatusCode: code,
+		Message:    message,
+	}
+}
+
+func (e *RequestError) Error() string {
+	if e.base != nil {
+		return e.base.Error()
+	}
+	return fmt.Sprintf("%d: %s", e.StatusCode, e.Message)
 }
 
 func New(config *ServerConfig) lib.Server {
-	logger := logging.New(&logging.LoggerConfig{
-		Name: "Controller",
-	})
+	base := lib.NewBaseServer("Controller")
 	cacheClient := config.CacheClient
 	if cacheClient == nil {
 		cacheAddr := config.CacheAddr
 		if cacheAddr == "" {
-			logger.Info("Cache address not set. Caching capability disabled.")
+			base.Logger.Info("Cache address not set. Caching capability disabled.")
 		} else {
 			cacheClient = NewProxyClient(config.CacheAddr)
 		}
 	}
 	webAddr := config.WebAddr
 	if webAddr == "" {
-		logger.Info("Web address not set. Static web capability disabled.")
+		base.Logger.Info("Web address not set. Static web capability disabled.")
 	}
 	client := config.DefaultClient
 	if client == nil {
@@ -62,27 +83,27 @@ func New(config *ServerConfig) lib.Server {
 	}
 
 	return &Server{
-		base:           lib.NewBaseServer(logger.Name),
+		base:           base,
 		config:         config,
 		client:         client,
 		cache:          cacheClient,
 		cacheAvailable: cacheClient != nil,
 		lock:           &sync.Mutex{},
-		logger:         logger,
+		upgrader:       &websocket.Upgrader{},
 	}
 }
 
 func (s *Server) Open(addr string) (net.Listener, error) {
 	if s.CacheAvailable() {
-		s.logger.Infof("Controller service at %s -> Cache service at %s", addr, s.config.CacheAddr)
+		s.base.Logger.Infof("Controller service at %s -> Cache service at %s", addr, s.config.CacheAddr)
 	}
 	if s.WebAvailable() {
 		if s.config.WebHost == "" {
 			hostname := httpHelpers.AddrToHost(addr)
-			s.logger.Infof("Web hostname not set. Using the default %s.", hostname)
+			s.base.Logger.Infof("Web hostname not set. Using the default %s.", hostname)
 			s.config.WebHost = hostname
 		}
-		s.logger.Infof("Controller service at %s -> Web server at %s", addr, s.config.WebAddr)
+		s.base.Logger.Infof("Controller service at %s -> Web server at %s", addr, s.config.WebAddr)
 	}
 	return s.base.Open(addr, s.serve)
 }
@@ -111,7 +132,13 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	defer func() {
 		if r := recover(); r != nil {
 			err := r.(error)
-			httpHelpers.WriteServerError(w, 503, "Internal server error", err)
+			code := 503
+			message := "Internal server error"
+			if reqErr, ok := r.(*RequestError); ok {
+				code = reqErr.StatusCode
+				message = reqErr.Message
+			}
+			httpHelpers.WriteServerError(s.base.Logger, w, code, message, err)
 		}
 		req.Body.Close()
 	}()
@@ -119,39 +146,23 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 }
 
 func (s *Server) ServeHTTPUnsafe(w http.ResponseWriter, req *http.Request) {
-	defer req.Body.Close()
-
-	u := httpHelpers.ParseURL(req)
-	hostname := u.Hostname()
-
-	c := s.client
-	if s.WebAvailable() && hostname == s.config.WebHost {
-		httpHelpers.LogRequest(s.logger, req, "Static web access")
-		u.Host = s.config.WebAddr
-	} else if strings.HasSuffix(hostname, ".granbluefantasy.jp") {
-		// Hostname starting with 'game-a' usually meant for loading asset files
-		if s.CacheAvailable() && strings.HasPrefix(hostname, "game-a") {
-			c = s.cache
-			httpHelpers.LogRequest(s.logger, req, "Cache access")
-		} else {
-			httpHelpers.LogRequest(s.logger, req, "Proxy access")
+	upgrade := req.Header.Get("Upgrade")
+	if upgrade == "websocket" {
+		ws, err := s.upgrader.Upgrade(w, req, nil)
+		if err != nil {
+			panic(err)
 		}
-	} else {
-		httpHelpers.LogRequest(s.logger, req, "Forbidden host")
-		httpHelpers.WriteError(w, 403, "Host not allowed")
+		s.ServeWebSocket(ws)
 		return
 	}
+	defer req.Body.Close()
 
-	clientReq := &http.Request{}
-	err := copier.Copy(clientReq, req)
+	res, err := s.ForwardRequest(req)
 	if err != nil {
-		panic(err)
-	}
-	clientReq.RequestURI = ""
-
-	res, err := c.Do(clientReq)
-	if err != nil {
-		httpHelpers.WriteServerError(w, 502, "Bad gateway", err)
+		if _, ok := err.(*RequestError); ok {
+			panic(err)
+		}
+		httpHelpers.WriteServerError(s.base.Logger, w, 502, "Bad gateway", err)
 		return
 	}
 	defer res.Body.Close()
@@ -178,6 +189,104 @@ func (s *Server) ServeHTTPUnsafe(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func (s *Server) ServeWebSocket(ws *websocket.Conn) {
+	defer func() {
+		r := recover()
+		if r == nil {
+			return
+		}
+		err := r.(error)
+		s.base.Logger.Error(err)
+
+		code := 503
+		message := "Internal server error"
+		if reqErr, ok := r.(*RequestError); ok {
+			code = reqErr.StatusCode
+			message = reqErr.Message
+		}
+
+		body := []byte(message)
+		err = s.WriteToWebSocket(ws, &http.Response{
+			StatusCode: code,
+			Body:       httpHelpers.NewBodyReader(body),
+		})
+		if err != nil {
+			s.base.Logger.Error(err)
+		}
+	}()
+	s.ServeWebSocketUnsafe(ws)
+}
+
+func (s *Server) ServeWebSocketUnsafe(ws *websocket.Conn) {
+	defer ws.Close()
+	req, err := s.ReadFromWebSocket(ws)
+	if err != nil {
+		panic(err)
+	}
+	res, err := s.ForwardRequest(req)
+	if err != nil {
+		panic(err)
+	}
+	err = s.WriteToWebSocket(ws, res)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (s *Server) ReadFromWebSocket(ws *websocket.Conn) (*http.Request, error) {
+	_, data, err := ws.ReadMessage()
+	if err != nil {
+		return nil, err
+	}
+	return s.UnmarshalRequest(data)
+}
+
+func (s *Server) WriteToWebSocket(ws *websocket.Conn, res *http.Response) error {
+	data, err := s.MarshalResponse(res)
+	if err != nil {
+		return err
+	}
+	return ws.WriteMessage(websocket.BinaryMessage, data)
+}
+
+func (s *Server) UnmarshalRequest(data []byte) (*http.Request, error) {
+	return nil, errors.New("Not implemented yet")
+}
+
+func (s *Server) MarshalResponse(res *http.Response) ([]byte, error) {
+	return nil, errors.New("Not implemented yet")
+}
+
+func (s *Server) ForwardRequest(req *http.Request) (*http.Response, error) {
+	u := httpHelpers.ParseURL(req)
+	hostname := u.Hostname()
+
+	c := s.client
+	if s.WebAvailable() && hostname == s.config.WebHost {
+		httpHelpers.LogRequest(s.base.Logger, req, "Static web access")
+		u.Host = s.config.WebAddr
+	} else if strings.HasSuffix(hostname, ".granbluefantasy.jp") {
+		// Hostname starting with 'game-a' usually meant for loading asset files
+		if s.CacheAvailable() && strings.HasPrefix(hostname, "game-a") {
+			c = s.cache
+			httpHelpers.LogRequest(s.base.Logger, req, "Cache access")
+		} else {
+			httpHelpers.LogRequest(s.base.Logger, req, "Proxy access")
+		}
+	} else {
+		httpHelpers.LogRequest(s.base.Logger, req, "Forbidden host")
+		return nil, NewRequestError(403, "Host not allowed", nil)
+	}
+
+	clientReq := &http.Request{}
+	err := copier.Copy(clientReq, req)
+	if err != nil {
+		panic(err)
+	}
+	clientReq.RequestURI = ""
+	return c.Do(clientReq)
+}
+
 func (s *Server) CacheAvailable() bool {
 	s.lock.Lock()
 	defer s.lock.Unlock()
@@ -194,7 +303,7 @@ func (s *Server) serve(l net.Listener) {
 	go s.startCacheHeartbeat()
 	err := http.Serve(l, s)
 	if err != nil {
-		// do nothing
+		s.base.Logger.Error(err)
 	}
 }
 
@@ -225,20 +334,20 @@ func (s *Server) startCacheHeartbeat() {
 func (s *Server) checkCacheHeartbeat(req *http.Request) bool {
 	res, err := s.cache.Do(req)
 	if err != nil {
-		s.logger.Infof("Cache Heartbeat: Got error '%s'", err)
+		s.base.Logger.Infof("Cache Heartbeat: Got error '%s'", err)
 		return false
 	}
 	b, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		s.logger.Infof("Cache Heartbeat: Got error while reading response '%s'", err)
+		s.base.Logger.Infof("Cache Heartbeat: Got error while reading response '%s'", err)
 		return false
 	}
 	text := strings.TrimSpace(string(b))
 	if text != "OK" {
-		s.logger.Infof("Cache Heartbeat: Expecting response 'OK', got '%s'", text)
+		s.base.Logger.Infof("Cache Heartbeat: Expecting response 'OK', got '%s'", text)
 		return false
 	}
-	s.logger.Infof("Cache Heartbeat: %s", text)
+	s.base.Logger.Infof("Cache Heartbeat: %s", text)
 	return true
 }
 
