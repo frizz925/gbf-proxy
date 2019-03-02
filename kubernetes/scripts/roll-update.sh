a21476117a52cfa73b892a2d868ae26dd09ab747
Izra Faturrahman <Frizz925@hotmail.com>
2019-03-02 18:32:01 +0700

    [k8s] Add rolling update script

---
+++ kubernetes/scripts/roll-update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+DEPLOYMENTS=(gbf-proxy gbf-proxy-controller gbf-proxy-cache)
+IMAGE_NAME=gbf-proxy
+IMAGE_TAG=$(git describe --always --long --dirty)
+
+for d in ${DEPLOYMENTS[@]}; do
+    kubectl set image deployments/$d $d=$IMAGE_NAME:$IMAGE_TAG
+done
