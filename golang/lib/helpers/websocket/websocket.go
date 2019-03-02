4d3ddc425b11749448c9ed4f266af3841cf3050c
Izra Faturrahman <Frizz925@hotmail.com>
2019-03-02 16:38:07 +0700

    [golang] Create structs for WebSocket

---
+++ golang/lib/helpers/websocket/websocket.go
@@ -0,0 +1,13 @@
+package websocket
+
+import httpHelpers "github.com/Frizz925/gbf-proxy/golang/lib/helpers/http"
+
+type Request struct {
+	ID      string
+	Payload httpHelpers.Request
+}
+
+type Response struct {
+	ID      string
+	Payload httpHelpers.Response
+}
