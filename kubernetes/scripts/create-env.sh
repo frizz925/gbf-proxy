e87e4b9e44d93973aea33616e5448b1b2d14b0e5
Izra Faturrahman <Frizz925@hotmail.com>
2019-03-03 17:53:36 +0700

    [k8s] Add PATH into env script

--- kubernetes/scripts/create-env.sh
+++ kubernetes/scripts/create-env.sh
@@ -2,6 +2,7 @@
 set -e
 
 TOKEN_CA_PATH=/etc/kubernetes/pki/ca.crt
+PATH=/opt/bin:$PATH
 
 TOKEN=$(kubeadm token list | awk 'NR==2{ print $1 }')
 TOKEN_CA_HASH=$(openssl x509 -in $TOKEN_CA_PATH -pubkey | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | awk '{ print $2 }')
