f6eec631e6a83903b36a5f3e22103287b4ae6328
Izra Faturrahman <Frizz925@hotmail.com>
2019-03-20 07:19:55 +0700

    [golang] Tidy up script a bit

--- golang/scripts/coverage.sh
+++ golang/scripts/coverage.sh
@@ -3,7 +3,7 @@ set -e
 PKG_EXC_PATTERN="(golang$|cmd)"
 
 cd $(dirname $0)/..
-COVER_PKGS=$(go list ./... | grep -v -E "$PKG_EXC_PATTERN" | tr '\n' ',')
+COVER_PKGS=$(go list ./... | grep -vE "$PKG_EXC_PATTERN" | tr '\n' ',')
 go test -race -coverprofile=coverage.txt \
     -covermode=atomic \
     -coverpkg=$COVER_PKGS ./...
